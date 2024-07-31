e5f5d237a27898c837dfdacb85403a88dfc8cf47
MatthieuRU <matthieu.rubert@gmail.com>
2024-07-31 14:06:29 +0200

    scoring

--- dash_apps/run_together/components/activity_analysis.py
+++ dash_apps/run_together/components/activity_analysis.py
@@ -72,6 +72,13 @@ def get_activity_analysis(extended_activity: ExtendedActivity):
         zone_pace_heart_rate_kpi[zone_pace]['average_pace'] = kpi_interval_tmp_pace[0]
         zone_pace_heart_rate_kpi[zone_pace]['average_heart_rate'] = kpi_interval_tmp_pace[1]
         zone_pace_heart_rate_kpi[zone_pace]['total_distance'] = kpi_interval_tmp_pace[2]
+        logging.info(
+            f"Zone {zone_pace} "
+            f"average_pace {zone_pace_heart_rate_kpi[zone_pace]['average_pace']} "
+            f"average_heart_rate {zone_pace_heart_rate_kpi[zone_pace]['average_heart_rate']} "
+            f"total_distance {zone_pace_heart_rate_kpi[zone_pace]['total_distance']} "
+
+        )
 
         for zone_heart_rate in zone_pace_heart_rate_kpi[zone_pace].keys():
             if zone_heart_rate not in ('average_heart_rate', 'average_pace', 'total_distance', 'intervals'):
@@ -85,18 +92,116 @@ def get_activity_analysis(extended_activity: ExtendedActivity):
                 zone_pace_heart_rate_kpi[zone_pace][zone_heart_rate]['average_heart_rate'] = kpi_interval_heart_rate[1]
                 zone_pace_heart_rate_kpi[zone_pace][zone_heart_rate]['total_distance'] = kpi_interval_heart_rate[2]
 
-    # logging.info(zone_pace_heart_rate_kpi['Marathon'])
-    to_display = 'Half-Marathon'
-    total_distance_first = zone_pace_heart_rate_kpi['Half-Marathon']['total_distance']
-    total_distance_second = zone_pace_heart_rate_kpi['10km']['total_distance']
+    # Get the two most important interval and then take how much zone it wasbpm_pace_mapping = {
+    zones_threshold = {
+            "100m": {
+                "threshold": 0.5,
+                "priority": 1
+            },
+            "5km": {
+                "threshold": 0.5,
+                "priority": 1
+            },
+            "10km": {
+                "threshold": 1,
+                "priority": 1
+            },
+            "Half-Marathon": {
+                "threshold": 2.1,
+                "priority": 1
+            },
+            "Marathon": {
+                "threshold": 4.2,
+                "priority": 1
+            },
+            "Active Jogging": {
+                "threshold": 0,
+                "priority": 0
+            },
+            "Slow Jogging": {
+                "threshold": 0,
+                "priority": 0
+            },
+            "Walk": {
+                "threshold": 0,
+                "priority": 0
+            },
+        }
 
-    analysis = f"🦶 {round(extended_activity.activity['distance'] / 1000, 2)} km: " \
-               f"{round(total_distance_first, 2)} km @ Half-Marathon Pace | " \
-               f"{round(total_distance_second, 2)} km @ 10km Pace |" \
-               f"| www.run-together.com"
+    zones_to_display = []
+
+    i = 0
+    # Loop per difficulties-priority eg: Marathon > Jogging
+    for pace_zone, zone_threshold in zones_threshold.items():
+        # In case there is not intervals at this pace
+        if pace_zone in list(zone_pace_heart_rate_kpi.keys()):
+            # Check there is already a priority one if yes we do not get anymore
+            if zone_threshold['priority'] == 0 and len(zones_to_display) > 0:
+                break
+
+            if zone_pace_heart_rate_kpi[pace_zone]['total_distance'] > zone_threshold['threshold']:
+                zones_to_display.append({
+                    'distance': zone_pace_heart_rate_kpi[pace_zone]['total_distance'],
+                    'pace_zone': pace_zone
+                })
+                i = i + 1
+
+                # I break to get only the first two highest value
+                if i == 2:
+                    break
+
+    sorted_zones_to_display = sorted(zones_to_display, key=lambda d: d['distance'], reverse=True)
+
+    analysis = f"🦶 {round(extended_activity.activity['distance'] / 1000, 2)} km: "
+    for zone_to_display in sorted_zones_to_display:
+        analysis = analysis + f" | {round(zone_to_display['distance'], 2)}" \
+                              f" km @ {zone_to_display['pace_zone']} Pace"
+
+    analysis = analysis + f" | www.run-together.com"
+
+    # Get the score for the most important interval calculated distance pace below / distance total
+    # Loop per difficulties-priority eg: Marathon > Jogging
+    longest_pace_zone = sorted_zones_to_display[0]
+    logging.info(f"Longest Pace zone for the activity: {longest_pace_zone} "
+                 f"with {round(longest_pace_zone['distance'], 2)}km")
+
+    distance_heart_rate_in_range = 0
+    for heart_rate_zone in list(zones_threshold.keys()):
+        logging.info(heart_rate_zone)
+        logging.info(zone_pace_heart_rate_kpi[longest_pace_zone['pace_zone']])
+        # In case there is not intervals at this heart Rate
+        if heart_rate_zone in list(zone_pace_heart_rate_kpi[longest_pace_zone['pace_zone']].keys()):
+            distance_heart_rate_in_range = distance_heart_rate_in_range + zone_pace_heart_rate_kpi[
+                longest_pace_zone['pace_zone']
+            ][heart_rate_zone]['total_distance']
+            # logging.info(
+            #     zone_pace_heart_rate_kpi[
+            #         longest_pace_zone['pace_zone']
+            #     ][heart_rate_zone]['total_distance']
+            # )
+
+        # If we arrive at the pace_zone we stop
+        if heart_rate_zone == longest_pace_zone['pace_zone']:
+            break
+
+    score = distance_heart_rate_in_range / sorted_zones_to_display[0]['distance'] * 100
+    logging.info(distance_heart_rate_in_range)
+    logging.info(sorted_zones_to_display[0]['distance'])
+    logging.info(score)
+    score_text = ""
+    if score > 50:
+        score_text = f"🟢 Congrats, {round(score, 2)}% of the distance run in " \
+                              f"{sorted_zones_to_display[0]['pace_zone']} Pace is equal or bellow the associated heart-rate. "
+    if score <= 50:
+        score_text = f"🔴 Be careful, {round(score, 2)}% of the distance run in " \
+                              f"{sorted_zones_to_display[0]['pace_zone']} Pace is above the associated heart-rate. "
 
     return html.Div(
-        children=analysis,
+        children=[
+            analysis,
+            html.Br(),
+            score_text,
+    ],
         className="h3",
         style={"text-align": "left"}
     )
--- dash_apps/run_together/utils/colors.py
+++ dash_apps/run_together/utils/colors.py
@@ -1,3 +1,4 @@
 class Colors:
     green = "#99CC66"
     orange = "#F39C12"
+
